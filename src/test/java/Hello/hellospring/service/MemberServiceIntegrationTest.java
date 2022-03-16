d8ec03a0401c8026b7d9f07217599d0bff22ec6c
minh0518 <rubics@naver.com>
2022-03-16 20:55:32 +0900

    [ADD]회원통합 테스트

--- src/test/java/Hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/Hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -7,35 +7,32 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemberServiceTest {
 
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest{
 
-    MemberService memberService;
+    //테스트케이스는 그냥 말 그대로 해당 코드만 테스트하면 되므로
+//편한거 쓰면 돼서 필드 주입을 이용해서 의존관계를 만들어 줍니다
+    @Autowired MemberService memberService;
 
-    MemoryMemberRepository repository;
-
-    @BeforeEach
-    public void beforeEach() {
-        repository = new MemoryMemberRepository();
-        memberService = new MemberService(repository);
-    }
-
-    @AfterEach
-    public void afterEach(){
-        repository.clearStore();
-    }
+    @Autowired MemberRepository repository;
+//빈에다가는 구현체말고 인터페이스를 등록해 두었으므로
 
     @Test
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
         //When
         Long saveId = memberService.join(member);
         //회원가입(join)하면 id값이 리턴 되므로
@@ -63,14 +60,7 @@ class MemberServiceTest {
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
 
 
-
     }
 
-    @Test
-    void findMembers() {
-    }
 
-    @Test
-    void findOne() {
-    }
 }
