164d371bbefba25ab28cef237b2664f293163ecb
minh0518 <rubics@naver.com>
2022-03-17 14:42:13 +0900

    [ADD]SpringData JPA

--- src/main/java/Hello/hellospring/SpringConfig.java
+++ src/main/java/Hello/hellospring/SpringConfig.java
@@ -7,33 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
-
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
                                 //@Autowired 기능
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+ //   @Bean
+  //  public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JDBCMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-
-        return new JpaMemberRepository(em);
+        //return new JpaMemberRepository(em);
         //리턴하는건 구현체
-    }
+  //  }
 }
