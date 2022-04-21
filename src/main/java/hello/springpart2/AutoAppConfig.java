aa74acc88b17d19b140c211b0a08eef757f89fe6
minh0518 <rubics@naver.com>
2022-04-21 16:32:26 +0900

    part2

--- src/main/java/hello/springpart2/AutoAppConfig.java
+++ src/main/java/hello/springpart2/AutoAppConfig.java
@@ -1,4 +1,7 @@
 package hello.springpart2;
+import hello.springpart2.member.MemberRepository;
+import hello.springpart2.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +14,11 @@ import org.springframework.context.annotation.FilterType;
         Configuration.class))
 
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
 
 }
