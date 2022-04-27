3f4c364bf59be71c9c90d074b55b8f6862f069cc
minh0518 <rubics@naver.com>
2022-04-27 13:30:24 +0900

    part2

--- src/main/java/hello/springpart2/discount/RateDiscountPolicy.java
+++ src/main/java/hello/springpart2/discount/RateDiscountPolicy.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
