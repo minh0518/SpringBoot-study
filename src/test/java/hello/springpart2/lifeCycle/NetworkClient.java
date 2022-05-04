f1df115d5efd897708b7fc5f082f357d6be7d3b3
minh0518 <rubics@naver.com>
2022-05-04 20:24:40 +0900

    part2

--- src/test/java/hello/springpart2/lifeCycle/NetworkClient.java
+++ src/test/java/hello/springpart2/lifeCycle/NetworkClient.java
@@ -1,31 +1,47 @@
 package hello.springpart2.lifeCycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean , DisposableBean {
+    //새로 implements
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출 , url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+//        connect();
+//        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
         this.url = url;
     }
 
-    //서비스 시작할 때 호출되는 메소드
     public void connect(){
         System.out.println("connect : "+url);
     }
 
-    // 연결이 된 상태에서 사용할 수 있는 메소드드
-   public void call(String message){
-       System.out.println("call"+url+"message ="+message);
-   }
+    public void call(String message){
+        System.out.println("call"+url+"message ="+message);
+    }
 
-    //서비스 종료시 호출되는 메소드
     public void disconnect(){
         System.out.println("close : "+url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //초기화 로직
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        //종료 로직
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
